de9a7af4c4590d3905dc4eaba13e16c06c16389e
심성헌 <dip.0cean@daum.net>
2020-04-22 18:17:31 +0900

    윤년 출력 문제
    코드 수정

--- Day 05/src/Task/Task04.java
+++ Day 05/src/Task/Task04.java
@@ -20,15 +20,15 @@ public class Task04 {
 		int year = sc.nextInt();
 
 //		비교 연산
-//		boolean a = (year % 400 == 0); // 400의 배수
-//		boolean b = (year % 100 == 0); // 100의 배수
-		boolean c = ((year % 400 == 0) == (year % 100 == 0)) && (year % 4 == 0); // a 와 b 둘 다 아닐 경우 4의 배수
+		boolean a = (year % 400 == 0); // 400의 배수인가
+		boolean b = (year % 100 != 0); // 100의 배수가 아닌가
+		boolean c = (a || b) && (year % 4 == 0); // a 와 b 둘 다 아닐 경우 4의 배수
 
 //		결과 출력				
-//		System.out.println("400의 배수인가? : 	" + a);
-//		System.out.println("100의 배수인가? : 	" + b);
-//		System.out.println("두 경우 다 아니면 4의 배수인가? : 	" + c);
-		System.out.println("입력한 값은 윤년인가요? : " + c);
+		System.out.println("400의 배수인가? : 		" + a);
+		System.out.println("100의 배수가 아닌가? : 	" + b);
+		System.out.println("두 경우 다 아니면 4의 배수인가? : 	" + c);
+		System.out.println("입력한 값은 윤년인가요? :	" + c);
 
 		sc.close();
 
