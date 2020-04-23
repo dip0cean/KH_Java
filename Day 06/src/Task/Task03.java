722c08f29d666649b428c888354909828f4f7447
심성헌 <dip.0cean@daum.net>
2020-04-23 17:32:56 +0900

    주차 금액 수정 전

--- Day 06/src/Task/Task03.java
+++ Day 06/src/Task/Task03.java
@@ -10,37 +10,29 @@ public class Task03 {
 //		1) 30분까지는 주차요금 무료
 //		2) 31분 부터 10분에 1000원
 //		3) 아무리 오래 주차해도 일 최대 요금은 만원
-
-		int inHour = 10; // 진입 시
-		int inMinute = 00; // 진입 분
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int inHour = sc.nextInt(); // 진입 시
+		int inMinute = sc.nextInt();; // 진입 분
 		int outHour = 10; // 진출 시
-		int outMinute = 33; // 진출 분
-		
-		int startTime = 30; //30분 이후 부터 결제 시작
-		int price = 1000; // freePrice 분당 가격
-		int freePrice = 10; // 10분 당 
-		
-		int freeMoney = (startTime/freePrice) * price;
-		
-		int inTime = (inHour*60)+inMinute; // 진입한 시간
-		int outTime = (outHour*60)+outMinute; // 진출한 시간
-		int sumTime = outTime - inTime; // 할인 전 총 사용 분
-		int tPrice = (sumTime/freePrice) * price;
-		
-		
-		if(sumTime >= 31) {
-			tPrice = tPrice;
-			if (tPrice >10000) {
-				tPrice = 10000;
-			}
+		int outMinute = 30; // 진출 분
+
+		int inTime = (inHour * 60) + inMinute; // 진입한 시간
+		int outTime = (outHour * 60) + outMinute; // 진출한 시간
+		int sumTime = outTime - inTime; // 할인 전 총 사용 시간
+
+		int price;
+
+		if (sumTime > 30 && sumTime < 130) { 
+			price = ((sumTime / 10 - 2) * 1000); // 30분 이후 출차
+		} else if (sumTime <= 30) {
+			price = 0; // 30분 이전 출차
 		} else {
-			tPrice = 0;
+			price = 10000; // 결제 금액 10000원 넘으면
 		}
-		
-
-		System.out.println(sumTime);
-		System.out.println(tPrice);
-
+		System.out.println("주차 시간 : " + sumTime + "분 입니다.");
+		System.out.println("결제 금액은 "+ price + "원 입니다.");
 	}
 
 }
