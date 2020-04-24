813e4c50aa06ecf06f2286f2e315d969c706fcfe
dip0cean <dip0cean@skuniv.ac.kr>
2020-04-24 10:17:09 +0900

    Day 06 최종 수정

--- Day 06/src/Task/Task03.java
+++ Day 06/src/Task/Task03.java
@@ -10,11 +10,13 @@ public class Task03 {
 			Scanner sc = new Scanner(System.in);
 
 //			입차 시간과 분		
+			System.out.println("입차 시간을 입력해주세요.");
 			int inHM = sc.nextInt();
 			int inH = inHM / 100; // 입차 시간
 			int inM = inHM % 100; // 입차 분
 
-//			출차 시간과 분		
+//			출차 시간과 분
+			System.out.println("출차 시간을 입력해주세요.");
 			int outHM = sc.nextInt();
 			int outH = outHM / 100; // 출차 시간
 			int outM = outHM % 100; // 출차 분
@@ -30,16 +32,17 @@ public class Task03 {
 			int inTime = (inH * 60) + inM; // 입차 시간
 			int outTime = (outH * 60) + outM; // 출차 시간
 			int sumTime = outTime - inTime; // 주차 시간
+			boolean payMent = sumTime > freeTime && sumTime < limitTime; // 10000원 미만으로 결제 되는 시간의 범위
 
 //			최종 요금		
 			int tPrice;
 
-			if (sumTime > freeTime && sumTime < limitTime && sumTime % priceTime != 0) {
+			if (payMent && sumTime % priceTime != 0) {
 				// 이용 시간 31분 부터 129분까지 그리고 이용시간의 10 나머지 값이 0이 아닐 경우
 
 				tPrice = ((sumTime / priceTime) - (freeTime / priceTime - 1)) * price;
 
-			} else if (sumTime > freeTime && sumTime < limitTime && sumTime % priceTime == 0) {
+			} else if (payMent && sumTime % priceTime == 0) {
 				// 이용 시간 31분 부터 129분까지 그리고 이용시간의 10 나머지 값이 0일 경우
 
 				tPrice = (sumTime / priceTime - (freeTime / priceTime)) * price;
