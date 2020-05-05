2f2657219442df322d179f21762993f47f608596
D0 <dip.0cean@daum.net>
2020-05-05 19:18:50 +0900

    Day 11 과제
    빙고 / 지뢰 찾기

--- Day 11/src/Task/Task02.java
+++ Day 11/src/Task/Task02.java
@@ -8,21 +8,21 @@ public class Task02 {
 //		5 X 5 빙고판 만들기
 //		1 ~ 25까지 랜덤하게 작성되도록 5 X 5 빙고판을 출력하라
 
-		// 입력
+//		 입력
 		Random r = new Random();
 		int gameLevel = 5;
 		int num = 1;
 		int[][] bingo = new int[gameLevel][gameLevel];
 		int save;
 
-		// [0][0] ~ [5][5]의 인덱스에 1 ~ 25까지의 숫자 넣기
+//		 [0][0] ~ [5][5]의 인덱스에 1 ~ 25까지의 숫자 넣기
 		for (int i = 0; i < bingo.length; i++) {
 			for (int k = 0; k < bingo[i].length; k++) {
 				bingo[i][k] = num++;
 			}
 		}
 
-		// 배열 랜덤 배치 (반으로 나눠 뒤집기)
+// 		배열 랜덤 배치 (반으로 나눠 뒤집기)
 		for (int i = 0; i < bingo.length / 2; i++) {
 			for (int k = 0; k < bingo[i].length; k++) {
 				int a = r.nextInt(bingo.length - bingo.length / 2) + bingo.length / 2;
@@ -36,14 +36,14 @@ public class Task02 {
 
 		// 출력
 		for (int i = 0; i < bingo.length; i++) {
-			System.out.print("\t[" + (char) (i + 97) + "]");
+			System.out.print("\t[" + (char) (i + 97) + "]\t");
 		}
 		System.out.println();
 
 		for (int i = 0; i < bingo.length; i++) {
 			System.out.print("[" + (char) (i + 65) + "]\t");
 			for (int k = 0; k < bingo[i].length; k++) {
-				System.out.print(bingo[i][k] + "\t");
+				System.out.print(bingo[i][k] + "\t|\t");
 			}
 			System.out.println("\n");
 		}
---
+++ Day 11/src/Task/Task04.java
@@ -0,0 +1,53 @@
+package Task;
+
+import java.util.Random;
+
+public class Task04 {
+	public static void main(String[] args) {
+//		9 X 9 크기의 지뢰 게임을 만드세요.
+//		지뢰의 개수는 총 20개, 자리는 랜덤
+
+//		입력
+		Random r = new Random();
+		int[][] mineGame = new int[9][9];
+		int count = 0;
+		int mine;
+
+//		연산		
+		for (int i = 0; i < mineGame.length; i++) {
+			for (int k = 0; k < mineGame[i].length; k++) {
+				mine = r.nextInt(100);
+				if (mine < 25) {
+					count++;
+					if (count <= 20) {
+						mineGame[i][k] = 9;
+					}
+				}
+			}
+		}
+		for (int i = 0; i < mineGame.length; i++) {
+			for (int k = 0; k < mineGame[i].length; k++) {
+				count = 0;
+				for (int x = i - 1; x <= i + 1; x++) {
+					for (int y = k - 1; y <= k + 1; y++) {
+						if (x < 0 || y < 0 || mineGame.length == x || mineGame[i].length == y) {
+						} else if (mineGame[i][k] != 9 && mineGame[x][y] == 9) {
+							count++;
+						}
+					}
+				}
+				mineGame[i][k] += count;
+			}
+		}
+
+// 		출력
+		for (int i = 0; i < mineGame.length; i++) {
+			System.out.print("[" + (char) (i + 65) + "]\t");
+			for (int k = 0; k < mineGame[i].length; k++) {
+				System.out.print(mineGame[i][k] + "  |  ");
+			}
+			System.out.println("\n------------------------------------------------------------");
+		}
+	}
+
+}
