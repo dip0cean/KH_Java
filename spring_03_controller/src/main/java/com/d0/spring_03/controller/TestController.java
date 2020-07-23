fc76b50cf92a91e2316923ed5e6b1daffc99e23f
D0 <dip.0cean@daum.net>
2020-07-23 15:09:13 +0900

    JSP 연결하기

--- spring_03_controller/src/main/java/com/d0/spring_03/controller/TestController.java
+++ spring_03_controller/src/main/java/com/d0/spring_03/controller/TestController.java
@@ -1,12 +1,19 @@
 package com.d0.spring_03.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class TestController {
 	// 메소드를 이용해서 컨트롤러 설정
-	
+
+	@RequestMapping(value = "/home")
 	public String home() {
-		return "go";
+		return "/WEB-INF/views/home.jsp";
+	}
+
+	@RequestMapping(value = "/test")
+	public String test() {
+		return "/WEB-INF/views/test.jsp";
 	}
 }
