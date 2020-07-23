a9fd26832f6835457baee91e04b9d771cfcee469
D0 <dip.0cean@daum.net>
2020-07-23 17:42:05 +0900

    Get / Post 방식으로 요청 + 응답

--- spring_05/src/main/java/com/d0/spring_05/controller/MemberController.java
+++ spring_05/src/main/java/com/d0/spring_05/controller/MemberController.java
@@ -1,11 +1,12 @@
 package com.d0.spring_05.controller;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.d0.spring_05.entity.MemberDTO;
 
 @Controller
 @RequestMapping("/member") // 공용으로 사용할 주소 지정
@@ -22,17 +23,39 @@ public class MemberController {
 		return "join"; // ViewResolver
 	}
 
+	// 기존 방식
 	/* @RequestMapping(value = "/join", method = RequestMethod.POST) */
+	/*
+	 * @PostMapping("/join") public String join_finish(HttpServletRequest req) {
+	 * String id = req.getParameter("id"); String pw = req.getParameter("pw");
+	 * String nick = req.getParameter("nick");
+	 * 
+	 * System.out.println("ID : " + id); System.out.println("PW : " + pw);
+	 * System.out.println("NICK : " + nick);
+	 * 
+	 * return "join_finish"; }
+	 */
+
+	// @RequestParam 사용
+	/*
+	 * @PostMapping("/join") public String join_finish(
+	 * 
+	 * @RequestParam String id,
+	 * 
+	 * @RequestParam String pw,
+	 * 
+	 * @RequestParam(required = false) String nick) { System.out.println("ID : " +
+	 * id); System.out.println("PW : " + pw); System.out.println("NICK : " + nick);
+	 * 
+	 * return "join_finish"; }
+	 */
+
+	// 클래스 주고 자동으로 받게 하기 : @ModelAttribute
 	@PostMapping("/join")
-	public String join_finish(HttpServletRequest req) {
-		String id = req.getParameter("id");
-		String pw = req.getParameter("pw");
-		String nick = req.getParameter("nick");
-		
-		System.out.println("ID : " + id);
-		System.out.println("PW : " + pw);
-		System.out.println("NICK : " + nick);
-		
+	public String join_finish(@ModelAttribute MemberDTO mdto) {
+		System.out.println("ID : " + mdto.getId());
+		System.out.println("PW : " + mdto.getPw());
+		System.out.println("NICK : " + mdto.getNick());
 		return "join_finish";
 	}
 }
---
+++ spring_05/src/main/java/com/d0/spring_05/entity/MemberDTO.java
@@ -0,0 +1,37 @@
+package com.d0.spring_05.entity;
+
+public class MemberDTO {
+	private String id;
+	private String pw;
+	private String nick;
+
+	public MemberDTO() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getPw() {
+		return pw;
+	}
+
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+
+	public String getNick() {
+		return nick;
+	}
+
+	public void setNick(String nick) {
+		this.nick = nick;
+	}
+
+}
