e7110ccef3836d6ea5e2c6821f69deddede5b794
d0 <dip.0cean@daum.net>
2020-07-27 16:38:35 +0900

    myBatis 설정하기

---
+++ mybatis_01/src/main/java/com/d0/mybatis/entity/StudentDTO.java
@@ -0,0 +1,38 @@
+package com.d0.mybatis.entity;
+
+public class StudentDTO {
+	private String name;
+	private int age;
+	private int score;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public int getScore() {
+		return score;
+	}
+
+	public void setScore(int score) {
+		this.score = score;
+	}
+
+	public StudentDTO() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	
+}
