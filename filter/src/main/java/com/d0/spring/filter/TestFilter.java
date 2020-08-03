d1889bbac8d0096da5559d6c3da9900321d667ee
d0 <dip.0cean@daum.net>
2020-08-03 16:41:27 +0900

    필터 적용

---
+++ filter/src/main/java/com/d0/spring/filter/TestFilter.java
@@ -0,0 +1,21 @@
+package com.d0.spring.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class TestFilter implements Filter {
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		System.out.println("filter test");
+		chain.doFilter(request, response);
+	}
+
+}
