894ac2de49b2fc5174ea2c687b7e45a13e3cbaff
d0 <dip.0cean@daum.net>
2020-08-03 20:03:58 +0900

    gallery 프로젝트 수

--- gallery/src/main/java/com/d0/gallery/entity/GalleryDTO.java
+++ gallery/src/main/java/com/d0/gallery/entity/GalleryDTO.java
@@ -2,6 +2,9 @@ package com.d0.gallery.entity;
 
 import org.springframework.web.multipart.MultipartFile;
 
+import lombok.Data;
+
+@Data
 public class GalleryDTO {
 	private long no;
 	private String title;
@@ -21,60 +24,5 @@ public class GalleryDTO {
 		this.setFtype(file.getContentType());
 	}
 
-	public long getNo() {
-		return no;
-	}
-
-	public void setNo(long no) {
-		this.no = no;
-	}
-
-	public String getTitle() {
-		return title;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-	public String getExplain() {
-		return explain;
-	}
-
-	public void setExplain(String explain) {
-		this.explain = explain;
-	}
-
-	public String getFname() {
-		return fname;
-	}
-
-	public void setFname(String fname) {
-		this.fname = fname;
-	}
-
-	public long getFsize() {
-		return fsize;
-	}
-
-	public void setFsize(long fsize) {
-		this.fsize = fsize;
-	}
-
-	public String getFtype() {
-		return ftype;
-	}
-
-	public void setFtype(String ftype) {
-		this.ftype = ftype;
-	}
-
-	public String getWhen() {
-		return when;
-	}
-
-	public void setWhen(String when) {
-		this.when = when;
-	}
 
 }
